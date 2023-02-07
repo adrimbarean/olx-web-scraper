9c0df19a738714ddadbd9d3e316a614965a8d4f9
Andrei Drimbarean <andrei.drimbarean@gmail.com>
2023-02-07 19:55:46 +0200

    main: ask user for file name
    Ask user for the CSV file name that will contain the scraped data.
    
    Signed-off-by: Andrei Drimbarean <andrei.drimbarean@gmail.com>

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from lib.reader import html_scraper
 from lib.writer import data_to_csv
 
 print("OLX Web Scraper!")
+fname = input("File name: ")
+
 input_url = input("URL: ")
 
 page_source = page_source_loader(input_url)
@@ -11,7 +13,7 @@ data = html_scraper(page_source=page_source)
 # This uploads the final data to mongodb, You
 # may also write the same to a csv file by making use of 
 # the data_to_csv function from the lib.writer module.
-data_to_csv("apt_data", data) 
+data_to_csv(fname, data) 
 
 
 
